28d20d4b52eab20ddd9c729969ee9c08c9de8752
Charmy Rosewolf <82834654+charmyrosewolf@users.noreply.github.com>
2024-04-20 23:31:42 -0400

    Pull some basic fights data from warcraft logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,28 @@
-import { getTime } from '@/utils';
+import { FightData, createReport } from '@/app/lib/utils';
 // Any images need the basePath
 // import Image from 'next/image';
 import styles from './page.module.css';
 
 export default async function Page() {
-  const data = await getTime();
+  const report = await createReport();
 
-  const date = new Date(data.datetime);
+  // console.log('REPORT DATA');
+  // console.log(report);
 
-  return (
-    <Home
-      time={`${date.toDateString()} ${date.toLocaleTimeString()} EST`}
-    ></Home>
-  );
+  return <Home report={report}></Home>;
 }
 
-type HomeProps = { time: any };
+type HomeProps = { report: FightData[] };
 
-async function Home({ time }: HomeProps) {
-  console.log('time', time);
+async function Home({ report }: HomeProps) {
+  const reportDataTest = report[0]?.name;
   return (
     <main className={styles.main}>
       <div className={styles.description}>This is a test</div>
 
-      <div className={styles.center}>{time}</div>
+      <div className={styles.center}>
+        {reportDataTest ? reportDataTest : 'NO DATA'}
+      </div>
 
       <div className={styles.grid}></div>
     </main>
