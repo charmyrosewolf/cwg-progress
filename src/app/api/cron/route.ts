1455821495bf9f37dd5f5992742ac7d3e79efe28
Charmy Rosewolf <82834654+charmyrosewolf@users.noreply.github.com>
2024-06-27 10:16:18 -0400

    should be post request

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,7 +1,7 @@
 import { sendDiscordUpdate } from '@/lib/report-progress.service';
 import type { NextRequest } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const authHeader = request.headers.get('authorization');
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response('Unauthorized', {
