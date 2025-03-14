286fbd459363141101242e121afdf595e5f40e87
Charmy Rosewolf <82834654+charmyrosewolf@users.noreply.github.com>
2025-03-14 16:28:17 -0400

    compile liberation of undermine data for TWW S2 and switch over

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -3,13 +3,17 @@ import * as DF_S2 from './df.s2';
 import * as DF_S3 from './df.s3';
 import * as DF_S4 from './df.s4';
 import * as TWW_S1 from './tww.s1';
+import * as TWW_S2 from './tww.s2';
 
 import * as GUILD_DATA from './guilds';
 import { isAfter } from 'date-fns/isAfter';
 
+const lastSeason = TWW_S1;
+const newSeason = TWW_S2;
+
 // TODO: could automate this even further by making an array of seasons
 function isNewSeason() {
-  return isAfter(new Date(), TWW_S1.SEASON_START_DATE);
+  return isAfter(new Date(), newSeason.SEASON_START_DATE);
 }
 
 const newRaid = isNewSeason();
@@ -18,10 +22,10 @@ export const GUILDS = GUILD_DATA.GUILDS;
 export const isCWG = GUILD_DATA.isCWG;
 
 // update this each season
-export const RAIDS = newRaid ? TWW_S1.RAIDS : DF_S4.RAIDS;
+export const RAIDS = newRaid ? newSeason.RAIDS : lastSeason.RAIDS;
 export const SEASON_END_DATE = newRaid
-  ? TWW_S1.SEASON_END_DATE
-  : DF_S4.SEASON_END_DATE;
+  ? newSeason.SEASON_END_DATE
+  : lastSeason.SEASON_END_DATE;
 export const SEASON_START_DATE = newRaid
-  ? TWW_S1.SEASON_START_DATE
-  : DF_S4.SEASON_START_DATE;
+  ? newSeason.SEASON_START_DATE
+  : lastSeason.SEASON_START_DATE;
