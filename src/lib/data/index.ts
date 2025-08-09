dcddb4874100ae8bd2b5f686424c61a9f65ccf42
Charmy Rosewolf <82834654+charmyrosewolf@users.noreply.github.com>
2025-08-09 10:23:57 -0400

    chore: prep for tww s3

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -4,12 +4,13 @@ import * as DF_S3 from './df.s3';
 import * as DF_S4 from './df.s4';
 import * as TWW_S1 from './tww.s1';
 import * as TWW_S2 from './tww.s2';
+import * as TWW_S3 from './tww.s3';
 
 import * as GUILD_DATA from './guilds';
 import { isAfter } from 'date-fns/isAfter';
 
-const lastSeason = TWW_S1;
-const newSeason = TWW_S2;
+const lastSeason = TWW_S2;
+const newSeason = TWW_S3;
 
 // TODO: could automate this even further by making an array of seasons
 function isNewSeason() {
